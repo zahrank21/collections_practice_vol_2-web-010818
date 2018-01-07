56d365d6fe12100ff4d93429ef870f8d4e6aea8a
zahrank21 <zahrank@gmail.com>
2018-01-07 09:23:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
-# your code goes here
+def begins_with_r(arr)
+  arr.all?{|el| el[0].include?("r")}
+
+end
+
+def contain_a(arr)
+  arr.select{|el| el.include?("a")}
+end
+
+def first_wa(arr)
+  arr.select{|el| el[0..1] == "wa"}.first
+end
+
+def remove_non_strings(arr)
+	arr.delete {|el| el.class != String}
+end
